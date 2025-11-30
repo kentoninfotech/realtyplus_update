07dcb5d8060f561c74fb6db1362077b710b03059
dqaplusapp <dqaplusapp@gmail.com>
2025-11-30 22:10:23 +0100

    jskjsdssdsdsdshkdsdsdswdsdsdsdsxxcxcxcc

--- resources/views/layouts/template_landing.blade.php
+++ resources/views/layouts/template_landing.blade.php
@@ -48,18 +48,18 @@
     <div class="container-fluid fixed-top px-0 wow fadeIn" data-wow-delay="0.1s">
         <div class="top-bar row gx-0 align-items-center d-none d-lg-flex">
             <div class="col-lg-6 px-5 text-start">
-                <small><i class="fa fa-map-marker-alt text-primary me-2"></i>Berger, Abuja</small>
+                <small><i class="fa fa-map-marker-alt text-primary me-2"></i>Mabushi, Abuja</small>
                 <small class="ms-4"><i class="fa fa-clock text-primary me-2"></i>9.00 am - 5.00 pm</small>
             </div>
             <div class="col-lg-6 px-5 text-end">
                 <small><i class="fa fa-envelope text-primary me-2"></i>info@royalrefuge.com.ng</small>
-                <small class="ms-4"><i class="fa fa-phone-alt text-primary me-2"></i>+234 803 507 3028</small>
+                <small class="ms-4"><i class="fa fa-phone-alt text-primary me-2"></i>+234(0) 908 741 5600</small>
             </div>
         </div>
 
         <nav class="navbar navbar-expand-lg navbar-light py-lg-0 px-lg-5 wow fadeIn" data-wow-delay="0.1s">
             <a href="index.html" class="navbar-brand ms-4 ms-lg-0">
-                <h1 class="display-5 text-primary m-0"><img src="{{asset('dist/landing/img/logo.png')}}" alt="kenton" height="50" width="100"></h1>
+                <h1 class="display-5 text-primary m-0"><img src="{{asset('dist/landing/img/logo.png')}}" alt="kenton" height="50" width="auto"></h1>
             </a>
             <button type="button" class="navbar-toggler me-4" data-bs-toggle="collapse"
                 data-bs-target="#navbarCollapse">
